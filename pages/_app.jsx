e742355dbbd37cc8880efcdb6483016087a9abd8
Tasnimul-Sharan <tasnimulsharan8962@gmail.com>
2026-09-06 11:40:35 +0600

    updated

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,16 +3,35 @@ import "../styles/globals.css";
 import { useLenis } from "../Hooks/useLenis";
 import Layout from "@/components/Layout";
 import { Analytics } from "@vercel/analytics/react";
+import { Noto_Sans_Bengali, Poppins } from "next/font/google";
 import "lenis/dist/lenis.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+
+const notoSansBengali = Noto_Sans_Bengali({
+  subsets: ["bengali"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-noto-bengali",
+});
 
 function MyApp({ Component, pageProps }) {
   useLenis();
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Analytics />
-    </Layout>
+    <div
+      className={`${poppins.variable} ${notoSansBengali.variable} site-font-root`}
+    >
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </div>
   );
 }
 
